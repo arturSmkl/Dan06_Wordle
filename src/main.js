fe75faeabec02da6d33e9d3ad2a2ac5088aedef4
Artur Smýkal <1133@student.itgymnazium.cz>
2025-05-24 18:55:50 +0200

    c1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css' // Icon set
+
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
